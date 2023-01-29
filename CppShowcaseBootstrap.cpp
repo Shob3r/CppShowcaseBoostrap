45891e5d2ab31f86608dea7aae0da9caf4f94494
Andrew Pinter <andrewpinter23@gmail.com>
2023-01-29 01:45:59 -0800

    Start work on libcurl + progressbar, Add the build configuration to the repository since it's not that large

--- CppShowcaseBootstrap.cpp
+++ CppShowcaseBootstrap.cpp
@@ -1,21 +1,37 @@
 // Most of these libraries are unused lol
-#include <iostream>
 #include <windows.h>
-#include <stdio.h>
-#include "include/curl/curl.h"
-#include "include/git2.h"
 #include <winhttp.h>
-#include <fstream>
 #include <direct.h>
 #include <string>
-#include <sys/stat.h>
 #include <filesystem>
 #include <shellapi.h>
-#include <ShlObj.h>
+#include <iostream>
+#include <stdio.h>
+#include <CommCtrl.h>
+#include "include/curl/curl.h"
+#include "include/git2.h"
 
 #pragma warning(disable:4996)
 #pragma comment(lib, "WinHTTP.lib")
 #pragma comment(lib, "git2.lib")
+
+static size_t WriteCallback(void* contents, size_t size, size_t nmemb, void* userp)
+{
+	// This function will be called by libcurl as it receives data from the download.
+	// We can use this opportunity to update the progress bar.
+
+	// First, calculate the total number of bytes received so far.
+	int total_bytes = size * nmemb;
+
+	// Next, update the progress bar.
+	// The progress bar is represented by a HWND (a handle to a window).
+	// We can use the SendMessage function to send a message to the progress bar
+	// telling it to update its position.
+	SendMessage((HWND)userp, PBM_SETPOS, total_bytes, 0);
+
+	// Return the number of bytes received, to let libcurl know how much data was processed.
+	return total_bytes;
+}
 
 size_t write_data(void* ptr, size_t size, size_t nmemb, FILE* stream) {
 	size_t written = fwrite(ptr, size, nmemb, stream);
@@ -33,14 +49,10 @@ int main() {
 
 	// Libgit2
 	git_libgit2_init();
-	git_repository *repo = NULL;
-	
+	git_repository* repo = NULL;
+
 	git_remote_callbacks callbacks = GIT_REMOTE_CALLBACKS_INIT;
 	git_clone_options clone_options = GIT_CLONE_OPTIONS_INIT;
-
-	// Libcurl
-
-	
 
 	// Check if app is installed
 	if (std::filesystem::exists(InstallDir) && std::filesystem::is_directory(InstallDir)) {
@@ -51,7 +63,7 @@ int main() {
 			std::cout << "Could not find the source code!";
 			return 1;
 		}
-		git_remote *remote = NULL;
+		git_remote* remote = NULL;
 
 		cloneCheck = git_remote_lookup(&remote, repo, "origin");
 		if (cloneCheck < 0) {
@@ -67,7 +79,6 @@ int main() {
 		git_remote_free(remote);
 		git_repository_free(repo);
 		git_libgit2_shutdown();
-
 
 		return 0;
 	}
@@ -85,18 +96,36 @@ int main() {
 				HINTERNET hConnect = WinHttpConnect(hSession, L"1.1.1.1",
 					INTERNET_DEFAULT_HTTPS_PORT, 0);
 				if (hConnect) {
-					int result = MessageBoxEx(NULL, L"Do you have Node.Js installed on your computer?", L"Pre-Start checks", MB_YESNO | MB_ICONQUESTION | MB_DEFBUTTON2 | MB_APPLMODAL | MB_SETFOREGROUND | MB_TOPMOST, LANG_NEUTRAL);
+					string NodeJsInstallDir = "C:\\Program Files\\nodejs";
 
-					if (result == IDYES) {
-						// The user will never select yes, they don't even know what Node.Js is lmao
-						MessageBoxEx(NULL, L"Good to know! this program will now download and install the source code", L"About to download", MB_OK | MB_ICONINFORMATION | MB_DEFBUTTON2 | MB_APPLMODAL | MB_SETFOREGROUND | MB_TOPMOST, LANG_NEUTRAL);
+					if (std::filesystem::exists(NodeJsInstallDir)) {
+						// Node.JS is installed, Continue to cloning the repo.
+						// No need to add anything in this section
 					}
-					else if (result == IDNO) {
-						// Always going to be what the user selects lmao
-						MessageBoxEx(NULL, L"This app will now download node.js to compile and run the app", L"About to download", MB_OK | MB_ICONASTERISK | MB_APPLMODAL | MB_SETFOREGROUND | MB_TOPMOST, LANG_NEUTRAL);
-					}
+					else {
+						// Node.JS is NOT installed, Download Node.js and clone the repo after it's installed
+						// Start Libcurl to download Node.Js
+						curl_global_init(CURL_GLOBAL_ALL);
+						CURL* curl = curl_easy_init();
+						curl_easy_setopt(curl, CURLOPT_URL, "https://nodejs.org/dist/v19.5.0/node-v19.5.0-x64.msi");
 
-					return 0;
+						// Do the funny progressbar thing
+						curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
+
+						HWND DownloadProgress = CreateWindowEx(0, PROGRESS_CLASS, NULL,
+							WS_CHILD | WS_VISIBLE,
+							10, 10, 200, 20, NULL,
+							NULL, GetModuleHandle(NULL), NULL);
+						SendMessage(DownloadProgress, PBM_SETRANGE, 0, MAKELPARAM(0, 100));
+						curl_easy_setopt(curl, CURLOPT_WRITEDATA, DownloadProgress);
+
+						// Download nodejs with libcurl fr this time
+						CURLcode res = curl_easy_perform(curl);
+
+						// Clean up libcurl, and run the installer file
+						curl_easy_cleanup(curl);
+						curl_global_cleanup();
+					}
 				}
 				else {
 					MessageBoxEx(NULL, L"Could not connect to the internet! run this file again once you have connected to the internet", L"Error!", MB_OK | MB_ICONERROR | MB_APPLMODAL | MB_SETFOREGROUND | MB_TOPMOST, LANG_NEUTRAL);
