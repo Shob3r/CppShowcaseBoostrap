a188cc8f58051250d1ff6f680cf7da4f4c4b963e
Andrew Pinter <andrewpinter23@gmail.com>
2023-01-29 17:31:51 -0800

    Libcurl function is done, extraction time next

--- CppShowcaseBootstrap.cpp
+++ CppShowcaseBootstrap.cpp
@@ -10,28 +10,13 @@
 #include <CommCtrl.h>
 #include "include/curl/curl.h"
 #include "include/git2.h"
+#include <zlib.h>
 
 #pragma warning(disable:4996)
 #pragma comment(lib, "WinHTTP.lib")
 #pragma comment(lib, "git2.lib")
 
-static size_t WriteCallback(void* contents, size_t size, size_t nmemb, void* userp)
-{
-	// This function will be called by libcurl as it receives data from the download.
-	// We can use this opportunity to update the progress bar.
-
-	// First, calculate the total number of bytes received so far.
-	int total_bytes = size * nmemb;
-
-	// Next, update the progress bar.
-	// The progress bar is represented by a HWND (a handle to a window).
-	// We can use the SendMessage function to send a message to the progress bar
-	// telling it to update its position.
-	SendMessage((HWND)userp, PBM_SETPOS, total_bytes, 0);
-
-	// Return the number of bytes received, to let libcurl know how much data was processed.
-	return total_bytes;
-}
+#define CHUNK 16384
 
 size_t write_data(void* ptr, size_t size, size_t nmemb, FILE* stream) {
 	size_t written = fwrite(ptr, size, nmemb, stream);
@@ -46,6 +31,16 @@ int main() {
 	string InstallDir = appData + "\\CppShowcase";
 	std::filesystem::path installPath(InstallDir);
 	std::error_code errorCode;
+	// Libcurl setup
+	CURL* curl;
+	CURLcode res;
+	FILE* fp;
+	const char* url = "https://nodejs.org/dist/v19.5.0/node-v19.5.0-win-x64.zip";
+	char nodeJsZipfile[FILENAME_MAX];
+	DWORD dwDownloaded = 0, dwTotalSize = 0;
+	// Get the current directory of the C++ applications
+	GetCurrentDirectoryA(sizeof(nodeJsZipfile), nodeJsZipfile);
+	strcat(nodeJsZipfile, "\\tools\\node.zip");
 
 	// Libgit2
 	git_libgit2_init();
@@ -104,28 +99,58 @@ int main() {
 					}
 					else {
 						// Node.JS is NOT installed, Download Node.js and clone the repo after it's installed
-						// Start Libcurl to download Node.Js
-						curl_global_init(CURL_GLOBAL_ALL);
-						CURL* curl = curl_easy_init();
-						curl_easy_setopt(curl, CURLOPT_URL, "https://nodejs.org/dist/v19.5.0/node-v19.5.0-x64.msi");
+						// Create the download progress bar
+						HWND ProgressBar = CreateWindowA(
+							"Static", "Downloading Prerequisites...",
+							WS_POPUP | WS_VISIBLE,
+							CW_USEDEFAULT, CW_USEDEFAULT, 150, 50,
+							NULL, NULL, NULL, NULL
+						);
+						// Start Libcurl
+						curl = curl_easy_init();
+						if (curl) {
+							fp = fopen(nodeJsZipfile, "wb");
+							if (!fp) {
+								cout << "Could not open file " << nodeJsZipfile << endl;
+								return 1;
+							}
 
-						// Do the funny progressbar thing
-						curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
+							// Pre-Download checks
+							curl_easy_setopt(curl, CURLOPT_URL, url);
+							curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, fwrite);
+							// Sets the file to write the data to
+							curl_easy_setopt(curl, CURLOPT_WRITEDATA, fp);
+							// Do shit with the progress bar
+							curl_easy_setopt(curl, CURLOPT_NOPROGRESS, 0L);
+							curl_easy_setopt(curl, CURLOPT_PROGRESSFUNCTION, [](void* clientp, double dltotal, double dlnow, double ultotal, double ulnow) -> int {
+								DWORD* params = (DWORD*)clientp;
+							params[0] = (DWORD)dlnow;
+							params[1] = (DWORD)dltotal;
+							return 0;
+								});
+							curl_easy_setopt(curl, CURLOPT_PROGRESSDATA, &dwDownloaded);
 
-						HWND DownloadProgress = CreateWindowEx(0, PROGRESS_CLASS, NULL,
-							WS_CHILD | WS_VISIBLE,
-							10, 10, 200, 20, NULL,
-							NULL, GetModuleHandle(NULL), NULL);
-						SendMessage(DownloadProgress, PBM_SETRANGE, 0, MAKELPARAM(0, 100));
-						curl_easy_setopt(curl, CURLOPT_WRITEDATA, DownloadProgress);
+							// Downloading time
+							res = curl_easy_perform(curl);
 
-						// Download nodejs with libcurl fr this time
-						CURLcode res = curl_easy_perform(curl);
+							// Clean up and end the LibCurl process
+							curl_easy_cleanup(curl);
+							fclose(fp);
+							// Close the prograssbar for the download
+							DestroyWindow(ProgressBar);
 
-						// Clean up libcurl, and run the installer file
-						curl_easy_cleanup(curl);
-						curl_global_cleanup();
+							//Check for download errors
+							if (res != CURLE_OK) {
+								std::cout << "Error while downloading: " << curl_easy_strerror(res) << endl;
+								return 1;
+							}
+						}
+						else {
+							std::cout << "Could not start libcurl!";
+							return 1;
+						}
 					}
+					// Download is done, time to extract with zlib
 				}
 				else {
 					MessageBoxEx(NULL, L"Could not connect to the internet! run this file again once you have connected to the internet", L"Error!", MB_OK | MB_ICONERROR | MB_APPLMODAL | MB_SETFOREGROUND | MB_TOPMOST, LANG_NEUTRAL);
